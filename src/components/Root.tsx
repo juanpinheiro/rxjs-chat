1b7057c2f3fbeb9e8d71f0a467b3b2b5f525d4fa
Juan Pinheiro <jtlpinheiro@gmail.com>
2018-06-13 03:54:20 -0300

    Chat structure

---
+++ src/components/Root.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import './Root.css';
+
+class Root extends React.Component {
+  public render() {
+    return (
+      <div className='chat-box'>
+        <div className='chat-header'>
+          <h1 className='chat-title'>RxJS Eduzz</h1><a className='chat-close'>×</a>
+        </div>
+        <div className='chat-body'>
+          <div className='chat-content'>
+            <div className='chat-item is-me'>
+              <p className='chat-message'>Teste</p>
+            </div>
+          <form className='chat-form'>
+            <input className='chat-form-input' type='text' placeholder='Enter your message'/>
+            <a className='button'>Send</a>
+          </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Root;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Root from './components/Root';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
